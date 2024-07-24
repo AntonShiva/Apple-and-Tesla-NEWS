2746473bd404fb2c35b4cee6f5f73ea1cd713ad4
AntonShiva <ryashinanthon@yandex.ru>
2024-07-24 20:21:14 +0300

    Initial Commit

---
+++ Apple and Tesla NEWS/Apple_and_Tesla_NEWSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_and_Tesla_NEWSApp.swift
+//  Apple and Tesla NEWS
+//
+//  Created by Anton Rasen on 24.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct Apple_and_Tesla_NEWSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apple and Tesla NEWSTests/Apple_and_Tesla_NEWSTests.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_and_Tesla_NEWSTests.swift
+//  Apple and Tesla NEWSTests
+//
+//  Created by Anton Rasen on 24.07.2024.
+//
+
+import Testing
+@testable import Apple_and_Tesla_NEWS
+
+struct Apple_and_Tesla_NEWSTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
