cc1305e731995d1b08a1fbbcdb06f1210e269bd7
AntonShiva <ryashinanthon@yandex.ru>
2024-08-19 13:30:56 +0300

    Добавил анимации и выравнивания в ScrollView и новую ссылку для Tesla

--- Apple and Tesla NEWS/View/MainScreen/Subviews/NewsSectionView.swift
+++ Apple and Tesla NEWS/View/MainScreen/Subviews/NewsSectionView.swift
@@ -21,22 +21,32 @@ struct NewsSectionView: View {
                 .padding(.top)
             
             ScrollView(.horizontal, showsIndicators: false) {
-                HStack(spacing: 15) {
-                    ForEach(articles, id: \.url) { article in
-                        NavigationLink {
-                            NewsInDetailView(article: article)
-                        } label: {
-                            NewsBlockView(article: article)
-                        }
-                        .buttonStyle(.plain)
-                    }
-                }
-                .padding(.horizontal)
-            }
-            .shadow(color: .gray, radius: 10, x: 3, y: 5)
-        }
-    }
-}
+                           LazyHStack(spacing: 15) {
+                               ForEach(articles, id: \.url) { article in
+                                   NavigationLink {
+                                       NewsInDetailView(article: article)
+                                   } label: {
+                                       NewsBlockView(article: article)
+                                           
+                                           .scrollTransition { view, phase in
+                                                                     view
+                                                                         .opacity(phase.isIdentity ? 1 : 0.4)
+                                                                         .scaleEffect(phase.isIdentity ? 1 : 0.9)
+                                                                         .offset()
+                                                                 }
+                                   }
+                                   .buttonStyle(.plain)
+                               }
+                           }
+                           .scrollTargetLayout()
+                           .padding(.horizontal)
+                       }
+                       .contentMargins(10, for: .scrollContent)
+                       .scrollTargetBehavior(.viewAligned)
+                       .shadow(color: .gray, radius: 10, x: 3, y: 5)
+                   }
+               }
+           }
 
 
 
